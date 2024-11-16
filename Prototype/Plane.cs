b56f3aafa7f66f1d89c796bb9ecfa83c23c81785
Devashish Dhamne <Devashish@villaplus.com>
2024-11-16 20:15:34 +0530

    Add project files.

---
+++ Prototype/Plane.cs
@@ -0,0 +1,20 @@
+﻿namespace Prototype
+{
+    public class Plane : IPlane
+    {
+        public int Capacity { get; set; }
+
+        public string Name { get; set; }
+
+        public Plane(string field, int capacity)
+        {
+            Name = field;
+            Capacity = capacity;
+        }
+
+        public Plane Clone()
+        {
+            return new Plane(this.Name, this.Capacity);
+        }
+    }
+}
---
+++ Prototype/PlaneCreator.cs
@@ -0,0 +1,14 @@
+﻿namespace Prototype
+{
+    public class PlaneCreator
+    {
+        public void Create()
+        {
+            var qatar = new Plane("Airbus A320", 450);
+
+            var emirates = qatar.Clone();
+
+            Console.WriteLine(emirates.Name); 
+        }
+    }
+}
